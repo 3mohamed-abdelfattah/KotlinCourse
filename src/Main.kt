2e0bf266183ff1b403903d5610b0e4eafb7858f0
MohamedMohamed <m01126165016@icloud.com>
2024-05-05 02:34:41 +0300

    Commit Test

--- src/Main.kt
+++ src/Main.kt
@@ -6,7 +6,7 @@ fun main() {
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, $name!")
 
-    for (i in 1..5) {
+    for (i in 1..10) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
