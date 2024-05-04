d93a6c049c25ab267fe21dec18dd672e454bd3af
MohamedMohamed <m01126165016@icloud.com>
2024-05-05 02:35:37 +0300

    Commit Test

--- src/Main.kt
+++ src/Main.kt
@@ -4,7 +4,7 @@ fun main() {
     val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, $name!")
+    println("Hello My Name, $name!")
 
     for (i in 1..10) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
