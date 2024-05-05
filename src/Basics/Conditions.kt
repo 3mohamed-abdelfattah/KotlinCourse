96400db0f97273d6a6f2b71f222eb2c0c6ea0ac6
MohamedMohamed <m01126165016@icloud.com>
2024-05-05 15:15:58 +0300

    When Expression

--- src/Basics/Conditions.kt
+++ src/Basics/Conditions.kt
@@ -1,9 +1,9 @@
 
 fun main() {
 
-    var price=5
-
     //If Condition
+
+    var price=5
 
     if (price>=10){
         price++
@@ -23,4 +23,33 @@ fun main() {
     var catNum = 3
     if (catNum>=3) println(-catNum) else println(--catNum)
 
+    println("------------------------------------")
+
+    //When Expression
+
+    val user=Users("Mohamed","Alaa")
+
+    when(user.name){
+
+        "Mohamed" -> println("I,m")
+        "Alaa" -> println("Friend")
+        else -> println("IDK")
+
+    }
+
+    println("------------------------------------")
+
+    when{
+
+        user.name =="Mohamed" && user.friend =="Alaa" -> {
+            println("Mohamed Alaa")
+        }
+        else -> {
+            println("Not Friends")
+        }
+
+    }
+
 }
+
+class Users(var name:String,var friend:String)
