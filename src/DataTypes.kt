8776d2c3a99cd9002d820a56d69af5e9749d0489
MohamedMohamed <m01126165016@icloud.com>
2024-05-05 03:15:43 +0300

    Data_Type

---
+++ src/DataTypes.kt
@@ -0,0 +1,29 @@
+fun main() {
+
+    val myByte : Byte = 8
+    val myShort : Short = 16
+    val myInt : Int = 12
+    val myLong : Long = 64
+    val myFloat : Float = 32.00F
+    val myDouble : Double = 64.00
+
+    println("Byte=$myByte ${myByte::class.java}")
+    println("Byte To Float=${myByte.toFloat()} ${myByte.toFloat()::class.java}")
+
+
+    val plusTwo = myInt.plus(20)
+    println(plusTwo)
+
+
+    val stringHello = "Hello_Mohamed"
+    println(stringHello)
+    println(stringHello.uppercase())
+    println(stringHello.lowercase())
+
+
+    val aChar : Char = 'M'
+    println(aChar)
+    //Unicode
+    println('\uFF00')
+
+}
