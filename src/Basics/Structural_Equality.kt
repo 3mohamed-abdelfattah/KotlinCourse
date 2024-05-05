07dfb538d6db41b28ee699d25646733ec9622efb
MohamedMohamed <m01126165016@icloud.com>
2024-05-05 14:49:06 +0300

    Structural_Equality

---
+++ src/Basics/Structural_Equality.kt
@@ -0,0 +1,27 @@
+
+fun main() {
+
+    val nameOne="Mohamed"
+    var nameTwo="Ahmed"
+
+    println(nameOne == nameTwo)
+
+    nameTwo="Mohamed"
+
+    println(nameOne.equals(nameTwo))
+
+    println(nameOne != nameTwo)
+
+    println("-----------------------------------")
+
+    var one =1
+    val two =2
+
+    println(one === two)
+
+    one = two
+
+    println(one === two)
+
+
+}
