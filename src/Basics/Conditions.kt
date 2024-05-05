6e9de3a93f1fcb4d951ca283870a92634bb9c8f7
MohamedMohamed <m01126165016@icloud.com>
2024-05-05 14:33:42 +0300

    If Condition

---
+++ src/Basics/Conditions.kt
@@ -0,0 +1,26 @@
+
+fun main() {
+
+    var price=5
+
+    //If Condition
+
+    if (price>=10){
+        price++
+        println(price)
+    }
+    else if(price<=5){
+        price =10
+        println(price)
+    }
+    else{
+        price--
+        println(price)
+    }
+
+    // If Single Line
+
+    var catNum = 3
+    if (catNum>=3) println(-catNum) else println(--catNum)
+
+}
