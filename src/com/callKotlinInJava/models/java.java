c84439bd7c1df4a300f70ad039129a2eb63e537c
MohamedMohamed <m01126165016@icloud.com>
2024-05-06 04:04:59 +0300

    Call Kotlin <--> Java

---
+++ src/com/callKotlinInJava/models/java.java
@@ -0,0 +1,16 @@
+package com.callKotlinInJava.models;
+
+import org.jetbrains.annotations.NotNull;
+
+public class java {
+
+    public String customer(String data){
+        return "data";
+    }
+
+    public void sayHi(@NotNull kotlin person){
+        person.setName("Mohamed");
+        person.getName();
+        person.printInfo();
+    }
+}
---
+++ src/com/callKotlinInJava/models/kotlin.kt
@@ -0,0 +1,12 @@
+package com.callKotlinInJava.models
+
+class kotlin (var name : String,cst :java){
+
+    init {
+        val test = cst.customer("data")
+    }
+
+    fun printInfo(){
+        println(name)
+    }
+}
