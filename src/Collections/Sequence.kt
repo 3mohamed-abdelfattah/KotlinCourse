d2c3614f572ad72ae5b2b39d06bceec17cbb9878
MohamedMohamed <m01126165016@icloud.com>
2024-05-08 00:51:35 +0300

    Sequence

--- src/Collections/Sequence.kt
+++ src/Collections/Sequence.kt
@@ -1,11 +1,11 @@
 fun main() {
-    val sequen = generateSequence(1) { it+1 }
+    val sequence = generateSequence(1) { it+1 }
         .take(1475700).toList()
 
-    println(sequen)
+    println(sequence)
     println("is done!!!!")
 
-    if (sequen.contains(50)){
+    if (sequence.contains(50)){
         println("50 is Found!!!")
     }
 }
