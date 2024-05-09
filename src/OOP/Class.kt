7a0ad187a963a35b799071c5bb4dd22d08f34631
MohamedMohamed <m01126165016@icloud.com>
2024-05-09 03:44:54 +0300

    OOP CLass

---
+++ src/OOP/Class.kt
@@ -0,0 +1,20 @@
+fun main() {
+    var user0 = User()
+    user0.name = "Rana"
+    user0.age = 21
+    user0.job = "Developer"
+    user0.gender = "Female"
+    user0.checkGender()
+    user0.printFullUser()
+    println("--------------------------")
+    var user1 = User()
+    user1.name = "Mohamed"
+    user1.age = 21
+    user1.job = "Developer"
+    user1.gender = "Male"
+    user1.checkGender()
+    user0.printFullUser()
+    println("--------------------------")
+
+}
+
