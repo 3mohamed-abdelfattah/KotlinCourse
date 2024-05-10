46ab7b07700a59885407ee94a9ecda5fcb6ccce2
MohamedMohamed <m01126165016@icloud.com>
2024-05-10 16:32:07 +0300

    Override

--- src/OOP/Class.kt
+++ src/OOP/Class.kt
@@ -1,19 +1,7 @@
 fun main() {
-    var user0 = User()
-    user0.name = "Rana"
-    user0.age = 21
-    user0.job = "Developer"
-    user0.gender = "Female"
-    user0.checkGender()
-    user0.printFullUser()
+    val user0 = User("Mohamed", "Developer", 18, "Male")
     println("--------------------------")
-    var user1 = User()
-    user1.name = "Mohamed"
-    user1.age = 21
-    user1.job = "Developer"
-    user1.gender = "Male"
-    user1.checkGender()
-    user0.printFullUser()
+    val user1 = User("Rasha", "Doctor", 17, "Female")
     println("--------------------------")
 
 }
--- src/OOP/User.kt
+++ src/OOP/User.kt
@@ -1,18 +1,27 @@
-class User {
+open class User(name: String, job: String, age: Int, gender: String) {
     var name: String = ""
     var job: String = ""
     var age: Int = 0
-    var gender: String = ""
+    var gender: String = "Female"
+        get() = "I,m : $field"
+        set(vlaue)= if (vlaue=="") println("$gender=HE Male")  else println("Not!!!")
 
-    fun printFullUser() {
-        println(
-            """
+    val fullData: Any = println(
+        """
         Name : $name
         Job : $job
         Age : $age
+        Gender : $gender
     """.trimIndent()
-        )
-    }
+    )
+        //Override
+        get() = "Full Data : $field"
+}
 
-    fun checkGender() = if (gender == "Male") println("Mr $name") else println("MS $name")
+fun main() {
+    val over = User("Morphemed", "Doctor", 31,"Male")
+    println(over.fullData)
+    println("--------------------------")
+    println(over.gender)
+
 }
