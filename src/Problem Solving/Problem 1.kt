8bb8edeeae446f3bde2c86c382f9875596c50834
MohamedMohamed <m01126165016@icloud.com>
2024-05-12 21:15:40 +0300

    Problem 1

--- src/Problem Solving/Problem 1.kt
+++ src/Problem Solving/Problem 1.kt
@@ -1,7 +1,31 @@
+/*Write a program that reads three integer numbers and prints true
+if the first number lies between the second and third one (inclusive). Otherwise,
+ it is to print false.
+The sorting order of the two last arguments can be any.
+
+
+======
+Sample Input:
+40
+30
+50
+==
+Sample Output :
+true
+======================================
+Sample Input :
+
+2
+3
+4
+Sample Output :
+false
+*/
+
 fun main() {
     println("Enter Three Num:")
 
-    val (numOne,numTwo,numThree)=IntArray(3){ readln().toInt() }
+    val (numOne, numTwo, numThree) = IntArray(3) { readln().toInt() }
 
     println(numOne in numTwo..numThree || numOne in numThree..numTwo)
 
